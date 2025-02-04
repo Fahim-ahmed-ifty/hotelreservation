421fe7a359b3360a2dee9258094ef72ea6e3c33d
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmailcom>
2025-02-04 22:44:42 +0600

    ✅`initially create room cards with props

--- app/components/RoomCard.tsx
+++ app/components/RoomCard.tsx
@@ -1,21 +1,32 @@
-import Image from 'next/image';
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import Button from "./Button";
 
 interface roomcardprops {
   text?: string;
-  img:string;
-  imgalt?:string;
-  details?:string;
+  img: string;
+  imgalt?: string;
+  details?: string;
 }
 
-const RoomCard = ({text,img,imgalt,details}:roomcardprops) => {
+const RoomCard = ({ text, img, imgalt, details }: roomcardprops) => {
   return (
-    <div className='w-[400px] h-[400px] p-6 bg-red-400 rounded-xl'>
-      <Image className='w-[350px] h-[200px] bg-red-500 rounded-lg' src={img} alt='images of room' width={350} height={200}/>
-        
-        <div className='mt-2 font-semibold'>
-          {text}</div></div>
-  )
-}
+    <div className="w-[400px] h-[400px] p-6 bg-blue-400 bg-opacity-80  rounded-xl">
+      <Image
+        className="w-[350px] h-[200px] rounded-lg"
+        src={img}
+        alt="images of room"
+        width={350}
+        height={200}
+      />
 
-export default RoomCard
+      <div className="mt-2 font-semibold">{text}</div>
+      <div>{details}</div>
+      <div className="mt-2 flex justify-center items-center">
+        <Button text="Book Now" bgColor="bg-orange-400" />
+      </div>
+    </div>
+  );
+};
+
+export default RoomCard;
