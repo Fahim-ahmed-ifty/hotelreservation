407c1ad2d6c86339635680b2b23e0509d8f7d02a
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmailcom>
2025-02-04 22:48:18 +0600

    ✅`initially create room cards with props and fixed button placemnets in card

--- app/components/RoomCard.tsx
+++ app/components/RoomCard.tsx
@@ -11,7 +11,7 @@ interface roomcardprops {
 
 const RoomCard = ({ text, img, imgalt, details }: roomcardprops) => {
   return (
-    <div className="w-[400px] h-[400px] p-6 bg-blue-400 bg-opacity-80  rounded-xl">
+    <div className="w-[400px] h-[400px] p-6 bg-blue-400 bg-opacity-80  rounded-xl flex flex-col justify-between">
       <Image
         className="w-[350px] h-[200px] rounded-lg"
         src={img}
@@ -21,7 +21,7 @@ const RoomCard = ({ text, img, imgalt, details }: roomcardprops) => {
       />
 
       <div className="mt-2 font-semibold">{text}</div>
-      <div>{details}</div>
+      <div className="flex-grow">{details}</div>
       <div className="mt-2 flex justify-center items-center">
         <Button text="Book Now" bgColor="bg-orange-400" />
       </div>
