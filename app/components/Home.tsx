1b299cd1ccf84b3dac1d2867d3a167d76ac23565
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-04 11:23:56 +0600

    ✅

---
+++ app/components/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div className='w-full h-screen flex flex-row'>
+			<div className='w-1/2 m-12 p-12'>
+				<h1 className='text-4xl font-bold'>Your Perfect Stay Awaits!</h1>
+				<h2 className='p-2'>
+					Book now and experience comfort, luxury, and convenience at
+					our exclusive hotel. Reserve your room today and make your
+					getaway unforgettable!
+				</h2>
+			</div>
+			<div className='w-1/2'>Right</div>
+		</div>
+	);
+};
+
+export default Home;
