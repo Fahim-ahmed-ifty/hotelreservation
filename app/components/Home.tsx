9ac3aa953350bdd220bbe2442cfdd004769aec0c
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-04 11:33:17 +0600

    ✅button added

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import Button from './Button';
 
 const Home = () => {
 	return (
 		<div className='w-full h-screen flex flex-row'>
 			<div className='w-1/2 m-12 p-12'>
-				<h1 className='text-4xl font-bold'>Your Perfect Stay Awaits!</h1>
+				<h1 className='text-4xl font-bold'>
+					Your Perfect Stay Awaits!
+				</h1>
 				<h2 className='p-2'>
 					Book now and experience comfort, luxury, and convenience at
 					our exclusive hotel. Reserve your room today and make your
 					getaway unforgettable!
 				</h2>
+        <Button text='Clicked For Reservation'
+        />
 			</div>
 			<div className='w-1/2'>Right</div>
 		</div>
