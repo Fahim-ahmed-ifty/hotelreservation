96252cc2e8ac1b7ca140aaa01070b93d63f63efe
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-05 15:33:33 +0600

    about us initially

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const AboutUs = () => {
-	return <div>AboutUs</div>;
+	return (
+		<div>
+			<h1>About Zetta Reservation</h1>
+			<p>
+				Zetta Reservation is your one-stop solution for hotel
+				bookings. Whether you’re traveling for business or leisure, we
+				offer a seamless experience to help you find the best
+				accommodations. Our platform connects you with a wide variety
+				of hotels, allowing you to reserve rooms at the best rates.
+				Enjoy a hassle-free booking experience with Zetta Reservation
+				and make your travel plans effortless.
+			</p>
+		</div>
+	);
 };
 
 export default AboutUs;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AboutUs from '../components/AboutUs';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
@@ -9,6 +10,7 @@ const page = () => {
 		<div className='w-screen h-screen overflow-y-auto scrollbar'>
 			<Navbar />
 			<Hero />
+			<AboutUs />
 			<RoomDetails />
 			<Footer />
 		</div>
