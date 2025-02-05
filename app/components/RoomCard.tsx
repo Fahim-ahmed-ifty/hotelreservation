03d4cd0523567094017228439d215cb207bb2b65
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-05 08:08:50 +0600

    ✅fixing  room card details

--- app/components/RoomCard.tsx
+++ app/components/RoomCard.tsx
@@ -1,32 +1,32 @@
-import Image from "next/image";
-import React from "react";
-import Button from "./Button";
+import Image from 'next/image';
+import React from 'react';
+import Button from './Button';
 
 interface roomcardprops {
-  text?: string;
-  img: string;
-  imgalt?: string;
-  details?: string;
+	text?: string;
+	img: string;
+	imgalt: string;
+	details?: string;
 }
 
 const RoomCard = ({ text, img, imgalt, details }: roomcardprops) => {
-  return (
-    <div className="w-[400px] h-[400px] p-6 bg-blue-400 bg-opacity-80  rounded-xl flex flex-col justify-between">
-      <Image
-        className="w-[350px] h-[200px] rounded-lg"
-        src={img}
-        alt="images of room"
-        width={350}
-        height={200}
-      />
+	return (
+		<div className='w-[400px] h-[400px] p-6 bg-blue-400 bg-opacity-80  rounded-xl flex flex-col justify-between'>
+			<Image
+				className='w-[350px] h-[200px] rounded-lg'
+				src={img}
+				alt={imgalt}
+				width={350}
+				height={200}
+			/>
 
-      <div className="mt-2 font-semibold">{text}</div>
-      <div className="flex-grow">{details}</div>
-      <div className="mt-2 flex justify-center items-center">
-        <Button text="Book Now" bgColor="bg-orange-400" />
-      </div>
-    </div>
-  );
+			<div className='mt-2 font-semibold'>{text}</div>
+			<div className='flex-grow'>{details}</div>
+			<div className='mt-2 flex justify-center items-center'>
+				<Button text='Book Now' bgColor='bg-orange-400' />
+			</div>
+		</div>
+	);
 };
 
 export default RoomCard;
