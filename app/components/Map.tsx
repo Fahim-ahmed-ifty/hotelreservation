4238247ca062ea2f97476ab477e99800bfbc8229
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-06 09:12:23 +0600

    initially add map

---
+++ app/components/Map.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import 'leaflet/dist/leaflet.css';
+import dynamic from 'next/dynamic';
+import React from 'react';
+
+const MapContainer = dynamic(
+	() => import('react-leaflet').then(mod => mod.MapContainer),
+	{ ssr: false }
+);
+const TileLayer = dynamic(
+	() => import('react-leaflet').then(mod => mod.TileLayer),
+	{ ssr: false }
+);
+const Marker = dynamic(
+	() => import('react-leaflet').then(mod => mod.Marker),
+	{ ssr: false }
+);
+const Popup = dynamic(
+	() => import('react-leaflet').then(mod => mod.Popup),
+	{ ssr: false }
+);
+
+interface MapProps {
+	pinned: [number, number];
+}
+
+const Map: React.FC<MapProps> = ({ pinned }) => {
+	return (
+		<MapContainer
+			center={pinned}
+			zoom={13}
+			style={{ height: '400px', width: '100%' }}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+			/>
+			<Marker position={pinned}>
+				<Popup>
+					Location: {pinned[0]}, {pinned[1]}
+				</Popup>
+			</Marker>
+		</MapContainer>
+	);
+};
+
+export default Map;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,6 +3,7 @@ import AboutUs from '../components/AboutUs';
 import CustomerSay from '../components/CustomerSay';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
+import Map from '../components/Map';
 import Navbar from '../components/Navbar';
 import RoomDetails from '../components/RoomDetails';
 
@@ -14,6 +15,12 @@ const page = () => {
 			<AboutUs />
 			<RoomDetails />
 			<CustomerSay />
+			<div className='m-12 p-12'>
+				<h2 className='text-xl font-bold mb-4'>
+					Our Location in Dhaka
+				</h2>
+				<Map pinned={[23.8103, 90.4125]} />
+			</div>
 			<Footer />
 		</div>
 	);
