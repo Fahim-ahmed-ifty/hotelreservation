9c19e14fbf92ed008305d56babb5ac8eea703158
fahim-ahmed-ifty <fahimahmediftyzettabyte@gmail.com>
2025-02-08 12:10:53 +0600

    contact us page fixed some issues

--- app/components/ContactPageLeft.tsx
+++ app/components/ContactPageLeft.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { FaMapMarkerAlt, FaPhone } from 'react-icons/fa';
 
-const ContactPageLeft = () => {
-  return (
-    <div>ContactPageLeft</div>
-  )
+interface Location {
+	name: string;
+	address: string;
+	phone: string;
 }
 
-export default ContactPageLeft
+interface ContactPageLeftProps {
+	locations: Location[];
+}
+
+const ContactPageLeft = ({ locations }: ContactPageLeftProps) => {
+	return (
+		<div className='p-6 mt-12 flex flex-col'>
+			<h2 className='text-xl font-semibold mb-4 flex justify-center items-center'>
+				Our Locations
+			</h2>
+			<ul className='space-y-4'>
+				{locations.map((location, index) => (
+					<li key={index} className='p-4 bg-white rounded-lg shadow'>
+						<h3 className='text-lg font-bold flex items-center gap-2'>
+							<FaMapMarkerAlt className='text-red-500' />{' '}
+							{location.name}
+						</h3>
+						<p className='text-gray-700 mt-1 flex items-center gap-2'>
+							<FaMapMarkerAlt className='text-blue-500' />{' '}
+							{location.address}
+						</p>
+						<p className='text-gray-700 mt-1 flex items-center gap-2'>
+							<FaPhone className='text-green-500' /> {location.phone}
+						</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default ContactPageLeft;
